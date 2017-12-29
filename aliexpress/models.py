1cf3179711f98ee3ed713a358f2f4226aee4aed2
Jabrail <lezgintsev13@yandex.ru>
2017-12-29 15:40:39 +0300

    Created models.

--- aliexpress/models.py
+++ aliexpress/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Price(models.Model):
+
+    price = models.IntegerField(max_length=999999)
+
+
+class Rate(models.Model):
+
+    rate = models.IntegerField(max_length=100)
+
+
+class Images(models.Model):
+
+    images = models.ImageField()
+
+
+class Goods(models.Model):
+
+    name = models.CharField(max_length=200)
+
+    category = models.CharField(max_length=200)
+
+    description = models.CharField(max_length=1000)
+
+    images = models.ForeignKey(Images)
+
+    rate = models.ForeignKey(Rate)
+
+    price = models.ForeignKey(Price)

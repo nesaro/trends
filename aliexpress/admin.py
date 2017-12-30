bb16386f6d97329d002c4dfdb64f2e39cf45d0af
Jabrail <lezgintsev13@yandex.ru>
2017-12-30 19:17:33 +0300

    seems like everythins works correctly

--- aliexpress/admin.py
+++ aliexpress/admin.py
@@ -1,7 +1,33 @@
 from django.contrib import admin
 from aliexpress import models as ali_models
+from aliexpress import models
+
+
+class ImagesInlineModel(admin.StackedInline):
+
+    model = models.Image
+
+    extra = 1
+
+
+class PriceInlineModel(admin.TabularInline):
+
+    model = models.Price
+    extra = 1
+
+
+class ProductAdminModel(admin.ModelAdmin):
+
+    fieldsets = [
+
+        ('Main info', {'fields': ['name', 'description', 'rate']}),
+
+    ]
+
+    inlines = [ImagesInlineModel, PriceInlineModel]
+
 
 admin.site.register(ali_models.Price)
 admin.site.register(ali_models.Image)
-admin.site.register(ali_models.Product)
+admin.site.register(ali_models.Product, ProductAdminModel)
 admin.site.register(ali_models.Rate)
--- aliexpress/models.py
+++ aliexpress/models.py
@@ -6,18 +6,6 @@ class Rate(models.Model):
     rate = models.IntegerField()
 
 
-class Image(models.Model):
-
-    image = models.ImageField()
-
-
-class Price(models.Model):
-
-    price = models.DecimalField(decimal_places=3, max_digits=9999999)
-
-    date_time = models.DateTimeField()
-
-
 class Category(models.Model):
 
     category = models.CharField(max_length=100)
@@ -29,11 +17,26 @@ class Product(models.Model):
 
     description = models.TextField()
 
-    image = models.ForeignKey(Image)
-
     rate = models.ForeignKey(Rate)
-
-    price = models.ForeignKey(Price)
 
     def __str__(self):
         return self.name
+
+
+class Price(models.Model):
+
+    price = models.DecimalField(decimal_places=3, max_digits=9999999)
+
+    date_time = models.DateTimeField()
+
+    product = models.ForeignKey(Product)
+
+
+class Image(models.Model):
+
+    image = models.ImageField()
+
+    product = models.ForeignKey(Product)
+
+    def __str__(self):
+        return self.image.name

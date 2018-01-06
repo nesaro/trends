eeff587c8fb2eedf2428a6e43094a6750feb355b
Jabrail <lezgintsev13@yandex.ru>
2018-01-06 15:16:40 +0300

    get exception

--- aliexpress/tests.py
+++ aliexpress/tests.py
@@ -9,30 +9,29 @@ from django.utils import timezone
 
 class AliexpressModelsTesting(TestCase):
 
+    def setUp(self):
+        self.rate = models.Rate(rate=9.6)
+        self.category = models.Category(category='Books')
+        self.product = models.Product(name='Gold', description="Scrooge McDuck's gold",
+                                      rate=self.rate,
+                                      category=self.category)
+        self.price = models.Price(price=676.50, created_at=timezone.now(), product=self.product)
+
     def test_pollute_Product_model(self):
-        new_one = models.Product(name='Gold', description="Scrooge McDuck's gold", rate=self.test_pollute_Rate_model(),
-                                 category=self.test_pollute_Category_model())
-        self.assertTrue(new_one)
-        self.assertIsNone(new_one.save())
-        return new_one
+        self.assertTrue(self.product)
+        self.assertIsNone(self.product.save())
 
     def test_pollute_Rate_model(self):
-        new_one = models.Rate(rate=9.6)
-        self.assertTrue(new_one)
-        self.assertIsNone(new_one.save())
-        return new_one
+        self.assertTrue(self.rate)
+        self.assertIsNone(self.rate.save())
 
     def test_pollute_Category_model(self):
-        new_one = models.Category(category='Books')
-        self.assertTrue(new_one)
-        self.assertIsNone(new_one.save())
-        return new_one
+        self.assertTrue(self.category)
+        self.assertIsNone(self.category.save())
 
     def test_pollute_Price_model(self):
-        new_one = models.Price(price=676.50, date_time=timezone.now(), product=self.test_pollute_Product_model())
-        self.assertTrue(new_one)
-        self.assertIsNone(new_one.save())
-        return new_one
+        self.assertTrue(self.price)
+        self.assertIsNone(self.price.save())
 
     # def test_pollute_Image_model(self):
     #     new_one = models.Image(image=Image.open(os.path.join(

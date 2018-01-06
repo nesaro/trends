e8526c2e1c865bda7f7f3b2d530769a84baaf8b4
Jabrail <lezgintsev13@yandex.ru>
2018-01-06 15:19:17 +0300

    properly works

--- aliexpress/tests.py
+++ aliexpress/tests.py
@@ -17,24 +17,9 @@ class AliexpressModelsTesting(TestCase):
                                       category=self.category)
         self.price = models.Price(price=676.50, created_at=timezone.now(), product=self.product)
 
-    def test_pollute_Product_model(self):
-        self.assertTrue(self.product)
-        self.assertIsNone(self.product.save())
-
-    def test_pollute_Rate_model(self):
-        self.assertTrue(self.rate)
+    def saving_entries(self):
         self.assertIsNone(self.rate.save())
-
-    def test_pollute_Category_model(self):
-        self.assertTrue(self.category)
         self.assertIsNone(self.category.save())
-
-    def test_pollute_Price_model(self):
-        self.assertTrue(self.price)
+        self.assertIsNone(self.product.save())
         self.assertIsNone(self.price.save())
 
-    # def test_pollute_Image_model(self):
-    #     new_one = models.Image(image=Image.open(os.path.join(
-    #         settings.BASE_DIR, r'aliexpress/for_test/waterfall-wallpaper-13.jpg')))
-    #     self.assertTrue(new_one)
-    #     self.assertIsNone(new_one.save())

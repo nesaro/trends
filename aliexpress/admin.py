f788cab21666bf1aab749707a4591ae75a2bf5bf
Jabrail <lezgintsev13@yandex.ru>
2018-01-09 14:15:26 +0300

    deleted unnecessary imports

--- aliexpress/admin.py
+++ aliexpress/admin.py
@@ -1,18 +1,17 @@
 from django.contrib import admin
 from aliexpress import models as ali_models
-from aliexpress import models
 
 
 class ImagesInlineModel(admin.StackedInline):
 
-    model = models.Image
+    model = ali_models.Image
 
     extra = 1
 
 
 class PriceInlineModel(admin.TabularInline):
 
-    model = models.Price
+    model = ali_models.Price
     extra = 1
 
 
@@ -31,4 +30,4 @@ admin.site.register(ali_models.Price)
 admin.site.register(ali_models.Image)
 admin.site.register(ali_models.Product, ProductAdminModel)
 admin.site.register(ali_models.Rate)
-admin.site.register(models.Category)
+admin.site.register(ali_models.Category)

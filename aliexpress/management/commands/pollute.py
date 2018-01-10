9002a2c7b5ac9903e77c0d7269efe56b500b417b
Jabrail <lezgintsev13@yandex.ru>
2018-01-10 17:36:09 +0300

    I've started working for data polluting issue

--- aliexpress/management/commands/pollute.py
+++ aliexpress/management/commands/pollute.py
@@ -1,5 +1,8 @@
 from django.core.management.base import BaseCommand, CommandError
 from aliexpress import models
+from aliexpress.utils import string_generator
+import random
+from django.utils import timezone
 
 
 class Command(BaseCommand):  # Simple command for data polluting
@@ -8,16 +11,30 @@ class Command(BaseCommand):  # Simple command for data polluting
 
     def handle(self, *args, **options):
 
-        self.product_rate = models.Rate(rate=9)
-        self.product_rate.save()
+        CATEGORIES_COUNT = 20
 
-        self.product_category = models.Category(category='Books')
-        self.product_category.save()
+        categories = [string_generator(10) for word in range(CATEGORIES_COUNT)]
 
-        self.new_product = models.Product(name='Magazine', description="The new 'Forbs' magazine", rate=self.product_rate,
-                       category=self.product_category)
-        self.new_product.save()
+        category_objects = models.Category.objects.bulk_create(
+            [models.Category(category=categories[product-1]) for product in range(CATEGORIES_COUNT)]
+        )
 
-        # self.stdout.write('%s %s' % (args, options)) # for debugging
+        self.stdout.write('Categories are created')
+
+        rate_objects = models.Rate.objects.bulk_create(
+            [models.Rate(rate=random.randint(0, 10)) for r in range(10)]
+        )
+
+        self.stdout.write('Rate objects are created')
+
+        for categ in category_objects:
+            categ.save()
+
+            choosen_rate = random.choice(rate_objects)
+            choosen_rate.save()
+
+            product_objects = models.Product.objects.bulk_create(
+                [models.Product(name=string_generator(12), description=string_generator(54),
+                                    rate=choosen_rate, category=categ) for p in range(100)])
 
         self.stdout.write(self.style.SUCCESS('Successfully populate data'))

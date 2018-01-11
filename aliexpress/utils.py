4c76cb85a2f1860de47f5f7fb13acaca4eed4033
Jabrail <lezgintsev13@yandex.ru>
2018-01-11 18:42:01 +0300

    refactoring

--- aliexpress/utils.py
+++ aliexpress/utils.py
@@ -1,18 +1,14 @@
 import random
+from string import ascii_letters
 
 
 def string_generator(max_length):
-    alphabet = ('a', 'b', 'c', 'd', 'e',
-                'f', 'g', 'h', 'i', 'j',
-                'k', 'l', 'm', 'n', 'o',
-                'p', 'q', 'r', 's', 't',
-                'u', 'v', 'w', 'x', 'y',
-                          'z')
+
     length = random.randint(1, abs(max_length))
 
     word = ''
 
     for w in range(length):
-        word += random.choice(alphabet)
+        word += random.choice(ascii_letters)
 
     return word
